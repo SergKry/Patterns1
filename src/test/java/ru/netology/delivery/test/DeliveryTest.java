9c5281fb57135470ae9ed476198898d22dc258a4
SergKry <necroastral@mail.ru>
2022-09-06 22:17:38 +0400

    исправление замечания

--- src/test/java/ru/netology/delivery/test/DeliveryTest.java
+++ src/test/java/ru/netology/delivery/test/DeliveryTest.java
@@ -1,10 +1,13 @@
 package ru.netology.delivery.test;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import ru.netology.delivery.data.DataGenerator;
+
+import java.time.Duration;
 
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.Condition.exactText;
@@ -21,6 +24,7 @@ class DeliveryTest {
 
     @Test
     @DisplayName("Should successful plan and replan meeting")
+
     void shouldSuccessfulPlanAndReplanMeeting() {
         var validUser = DataGenerator.Registration.generateUser("ru");
         var daysToAddForFirstMeeting = 4;
@@ -34,11 +38,16 @@ class DeliveryTest {
         $("[data-test-id=phone] input").setValue(validUser.getPhone());
         $("[data-test-id=agreement]").click();
         $(".button").click();
-        $(byText("Успешно!")).shouldBe(visible);
+        $(byText("Успешно!")).shouldBe(visible,Duration.ofSeconds(15));
         $("[data-test-id=date] .input__control").doubleClick().sendKeys(secondMeetingDate);
         $(".button").click();
         $$(".button").find(exactText("Перепланировать")).click();
         $(byText("Успешно!")).shouldBe(visible);
+        $("[data-test-id=success-notification] .notification__content")
+
+                .shouldBe(visible).should(exactText("Встреча успешно запланирована на " + secondMeetingDate));
+
+
         // TODO: добавить логику теста в рамках которого будет выполнено планирование и перепланирование встречи.
         // Для заполнения полей формы можно использовать пользователя validUser и строки с датами в переменных
         // firstMeetingDate и secondMeetingDate. Можно также вызывать методы generateCity(locale),
